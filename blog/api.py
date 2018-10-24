827156ebd53925bff09ea05cef8d8b3d7169bc03
DasGMA <dasgrigoma@gmail.com>
2018-10-24 13:34:02 -0700

    All done lalalalalalalala

--- blog/api.py
+++ blog/api.py
@@ -7,9 +7,18 @@ class PersonalBlogSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('title', 'body')
     
     def create(self, validated_data):
-        blog = PersonalBlog.objects.create(**validated_data)
+        user = self.context['request'].user
+        blog = PersonalBlog.objects.create(user = user, **validated_data)
         return blog
 
 class PersonalBlogViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalBlogSerializer
     queryset = PersonalBlog.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalBlog.objects.none()
+        else:
+            return PersonalBlog.objects.filter(user=user)
--- dasMA/settings.py
+++ dasMA/settings.py
@@ -38,16 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
+    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
